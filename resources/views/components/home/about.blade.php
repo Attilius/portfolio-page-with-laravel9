97636c6ec368cc423f38f6d21f136dc1c54b790d
Attilius <leo20@freemail.hu>
2022-04-17 20:23:56 +0200

    I modified about.blade.php

--- resources/views/components/home/about.blade.php
+++ resources/views/components/home/about.blade.php
@@ -1,3 +1,61 @@
-<div>
-    <!-- Be present above all else. - Naval Ravikant -->
-</div>
+<!-- ====== About Section Start ====== -->
+<section id="about" class="dark:bg-slate-800 pt-20 lg:pt-[120px] pb-12 lg:pb-[90px] overflow-hidden">
+    <div class="container">
+      <div class="flex flex-wrap justify-between items-center -mx-4">
+        <div class="w-full lg:w-6/12 px-4">
+          <div class="flex items-center -mx-3 sm:-mx-4">
+            <div class="w-full xl:w-1/2 px-3 sm:px-4">
+              <div class="py-3 sm:py-4">
+                <img
+                  src="{{ url('') }}"
+                  alt=""
+                  class="rounded-2xl w-full"
+                />
+              </div>
+              <div class="py-3 sm:py-4">
+                <img
+                  src="{{ url('') }}"
+                  alt=""
+                  class="rounded-2xl w-full"
+                />
+              </div>
+            </div>
+            <div class="w-full xl:w-1/2 px-3 sm:px-4">
+              <div class="my-4 relative z-10">
+                <img
+                  src="{{ url('') }}"
+                  alt=""
+                  class="rounded-2xl w-full"
+                />
+                <x-about-dots></x-about-dots>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="w-full lg:w-1/2 xl:w-5/12 px-4">
+          <div class="mt-10 lg:mt-0">
+            <span class="font-semibold text-lg text-primary mb-2 block">
+               <blockquote class="text-sm text-gray-500 italic py-2 px-3 border-l-4 border-orange-600">
+                   "A successful person is one who gets up in the morning, goes to bed at night and does what he likes in between."
+               </blockquote>
+            </span>
+            <h2 class="font-bold text-3xl sm:text-4xl dark:text-gray-200 mb-8">
+              About Me
+            </h2>
+            <p class="text-base dark:text-gray-400 mb-8">
+                After my college studies in 2020, because of established the situation I decided to pursue a career in programming. My interest is mostly focused on
+                <span class="text-orange-600 font-bold">PHP</span> and <span class="text-orange-600 font-bold">JavaScript</span>. <br />
+            </p>
+            <p class="text-base dark:text-gray-400 mb-8">
+                I've also found a new hobby in programming, because it's have many challenge. Problem solving has become a passion of mine over the years.
+               <br />
+            </p>
+            <x-button-link href="#" variant="orange">
+              View my channel
+            </x-button-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- ====== About Section End ====== -->
