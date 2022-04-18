1990a6934bf62e52b32a2a8632c1b4debd1a5bd9
Attilius <leo20@freemail.hu>
2022-04-18 19:52:16 +0200

    I modified navbar-item.blade.php

--- resources/views/layout/navbar-item.blade.php
+++ resources/views/layout/navbar-item.blade.php
@@ -2,7 +2,7 @@
     <a 
     href="{{ $href }}" 
     class="text-m font-medium text-emerald-600 py-2 flex hover:text-dark dark:text-gray-200 
-           hover:dark:text-emerald-600 lg:inline-flex lg:ml-6 xl:ml-12"
+           hover:dark:text-emerald-600 lg:inline-flex lg:ml-6 xl:ml-12 menuItem"
     >
         {{ $slot }}
     </a>
