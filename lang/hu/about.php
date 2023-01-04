ecc02a456f2f618b8cfabd53354b05592a74237e
Attilius <leo20@freemail.hu>
2023-01-04 18:24:57 +0100

    I added about.php for (hu)

---
+++ lang/hu/about.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "quote" => "Sikeres ember az, aki reggel felkel, este lefekszik, és a kettő között azt csinálja, amit szeret.",
+    "about_me" => "Rólam",
+    "description1" => "Főiskolai tanulmányaim után 2020-ban a kialakult helyzet miatt úgy döntöttem, hogy programozói karrierbe kezdek. Érdeklődésem főként a következő programnyelvekre irányul",
+    "and" => "és",
+    "description2" => "A programozásban új hobbit is találtam, mert sok kihívást rejt magában. A problémamegoldás az évek során szenvedélyemmé vált.",
+    "experience" => "Tapasztalatok megtekintése"
+];
