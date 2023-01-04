3b5435f7343e1738ec57b92b57cb4d469431f6e1
Attilius <leo20@freemail.hu>
2023-01-04 18:25:25 +0100

    I added hero.php for (en)

---
+++ lang/en/hero.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "name" => "My Name is Attila",
+    "i_am_a" => "I am a",
+    "profession" => "Web Developer /",
+    "introduce_myself" => "I have been programming since 2020, with web development being my main profile. I like to learn about different technologies and program languages, so that I can solve the given task in different ways, thus expanding my experience.",
+    "my_projects" => "My Projects",
+    "follow_me" => "Follow me on social media"
+];
