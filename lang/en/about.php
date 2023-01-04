180a4d6be4c52faeeebd5d56adefc672b76fb58d
Attilius <leo20@freemail.hu>
2023-01-04 18:24:46 +0100

    I added about.php for (en)

---
+++ lang/en/about.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "quote" => "A successful person is one who gets up in the morning, goes to bed at night and does what he likes in between.",
+    "about_me" => "About Me",
+    "description1" => "After my college studies in 2020, because of established the situation I decided to pursue a career in programming. My interest is mostly focused on",
+    "and" => "and",
+    "description2" => "I've also found a new hobby in programming, because it's have many challenge. Problem solving has become a passion of mine over the years.",
+    "experience" => "View my experience"
+];
