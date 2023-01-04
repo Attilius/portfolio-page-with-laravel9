fe63520c7fe8c6e2ead5d0f456f6fce5b5883f1e
Attilius <leo20@freemail.hu>
2023-01-04 18:25:14 +0100

    I added hero.php for (hu)

---
+++ lang/hu/hero.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "name" => "Attila vagyok",
+    "i_am_a" => "",
+    "profession" => "Webfejlesztő /",
+    "introduce_myself" => "2020 óta foglalkozom programozással, a fő profilom a webfejlesztés. Szeretek különböző technológiákat és programnyelveket megismerni, hogy az adott feladatot különböző módon oldhassam meg, így bővítve a tapasztalataimat.",
+    "my_projects" => "Projektjeim",
+    "follow_me" => "Kövess az alábbi közösségi médiákon"
+];
