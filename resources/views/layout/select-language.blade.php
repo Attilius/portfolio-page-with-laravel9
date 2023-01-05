c110430a7984e691991440ab3cc092c4aeef5da6
Attilius <leo20@freemail.hu>
2023-01-05 17:35:23 +0100

    I added select-language.blade.php

---
+++ resources/views/layout/select-language.blade.php
@@ -0,0 +1,11 @@
+<li>
+    <label for="countries"></label>
+    <select id="countries" class="ml-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                             focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-LightSlateGray-900
+                             dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
+                             dark:focus:border-blue-500">
+        <option value="en">English</option>
+        <option value="hu">Magyar</option>
+    </select>
+</li>
+
