37d9f9f1ebd6a2d86d912cee59df910348c93ee9
Attilius <leo20@freemail.hu>
2023-01-05 18:06:56 +0100

    I modified select-language.blade.php

--- resources/views/layout/select-language.blade.php
+++ resources/views/layout/select-language.blade.php
@@ -1,6 +1,6 @@
 <li>
     <label for="countries"></label>
-    <select id="countries" class="ml-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+    <select id="countries" class="ml-4 bg-gray-50 text-gray-900 text-sm
                              focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-LightSlateGray-900
                              dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
                              dark:focus:border-blue-500">
@@ -9,4 +9,3 @@
         @endforeach
     </select>
 </li>
-
