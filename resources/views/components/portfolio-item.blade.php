35c6f7a0e786fbd4fa81819cf412f6ce0e0a54f9
Attilius <leo20@freemail.hu>
2023-01-06 17:28:04 +0100

    I modified portfolio-item.blade.php

--- resources/views/components/portfolio-item.blade.php
+++ resources/views/components/portfolio-item.blade.php
@@ -21,7 +21,7 @@
       <h3 class="font-bold text-lg text-dark dark:text-gray-300 mb-4">
         {{ $title }}
       </h3>
-      <x-button-link :href="$github" variant="primary">View Details</x-button-link>
+      <x-button-link :href="$github" variant="primary">{{ __('portfolio-item.view_details') }}</x-button-link>
     </div>
   </div>
 </div>
