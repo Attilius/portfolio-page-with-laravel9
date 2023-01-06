4bb58487d40eb8fb3b959aa09b1ffec468eb9066
Attilius <leo20@freemail.hu>
2023-01-06 17:23:17 +0100

    I added contact.php for (en)

---
+++ lang/en/contact.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "contact_description" => "If you would like to contact me, you can do so here on this page using the options down below.",
+    "contact" => "Contact",
+    "me" => "me",
+    "email_address" => "Email Address",
+    "send_message" => "Send Message",
+    "name" => "Your Name",
+    "email" => "Your Email",
+    "message" => "Your Message"
+];
