34efdd9d4f827f04275b1d25851d139cdc062e65
Attilius <leo20@freemail.hu>
2023-01-06 17:51:22 +0100

    I modified contact.php for (hu)

--- lang/hu/contact.php
+++ lang/hu/contact.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    "contact_description" => "Ha kapcsolatba szeretne lépni velem, akkor azt itt, ezen az oldalon teheti meg az alábbi lehetőségek segítségével.",
-    "contact" => "Lépjen kapcsolatba",
+    "contact_description" => "Ha kapcsolatba szeretnél lépni velem, akkor azt itt, ezen az oldalon teheted meg az alábbi lehetőségek segítségével.",
+    "contact" => "Lépj kapcsolatba",
     "me" => "velem",
     "email_address" => "Email cím",
     "send_message" => "Üzenet küldése",
