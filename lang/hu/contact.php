38543736301e5f4862e954a6a2a47682d42d2c2c
Attilius <leo20@freemail.hu>
2023-01-06 17:23:28 +0100

    I added contact.php for (hu)

---
+++ lang/hu/contact.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "contact_description" => "Ha kapcsolatba szeretne lépni velem, akkor azt itt, ezen az oldalon teheti meg az alábbi lehetőségek segítségével.",
+    "contact" => "Lépjen kapcsolatba",
+    "me" => "velem",
+    "email_address" => "Email cím",
+    "send_message" => "Üzenet küldése",
+    "name" => "Név",
+    "email" => "Email cím",
+    "message" => "Üzenet szövege"
+];
