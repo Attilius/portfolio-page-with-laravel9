70644235d6e9bac6831b0da81df1a1e7d2ee7125
Attilius <leo20@freemail.hu>
2023-01-07 19:38:38 +0100

    I modified select-language.blade.php

--- resources/views/layout/select-language.blade.php
+++ resources/views/layout/select-language.blade.php
@@ -1,6 +1,6 @@
 <li>
-    <label for="countries"></label>
-    <select id="countries" class="ml-4 bg-gray-50 text-gray-900 text-sm
+    <label for="languages"></label>
+    <select id="languages" class="ml-4 bg-gray-50 text-gray-900 text-sm
                              focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-LightSlateGray-900
                              dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
                              dark:focus:border-blue-500">
