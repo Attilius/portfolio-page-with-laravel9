607ed398edb706955e64bee0ca7d40e4f9b1d588
Attilius <leo20@freemail.hu>
2023-01-08 20:16:23 +0100

    I modified select-language.blade.php

--- resources/views/layout/select-language.blade.php
+++ resources/views/layout/select-language.blade.php
@@ -1,12 +1,12 @@
 <li>
-    <form action="/hu" method="GET">
+    <form action="/:" method="GET">
         <label for="languages"></label>
-        <select onchange="this.form.submit()" id="languages" class="ml-4 bg-gray-50 text-gray-900 text-sm
+        <select onchange="this.form.submit()" name="lang" id="languages" class="ml-4 bg-gray-50 text-gray-900 text-sm
                              focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-LightSlateGray-900
                              dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
                              dark:focus:border-blue-500">
             @foreach($selectOptions as $option)
-                <x-layout.select-language-item :value="$option['value']">{{ $option['language_name'] }}</x-layout.select-language-item>
+                <x-layout.select-language-item :value="$option['value']" :selected="$option['selected']">{{ $option['language_name'] }}</x-layout.select-language-item>
             @endforeach
         </select>
     </form>
