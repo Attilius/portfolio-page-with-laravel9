06aefb4c21190d171e4ad8883bbfe3c8b211e770
Attilius <leo20@freemail.hu>
2023-01-09 20:09:13 +0100

    I modified select-language.blade.php

--- resources/views/layout/select-language.blade.php
+++ resources/views/layout/select-language.blade.php
@@ -1,9 +1,13 @@
 <li>
     <label for="languages"></label>
-    <select onchange="this.form.submit()" name="lang" id="languages" class="ml-4 bg-gray-50 text-gray-900 text-sm
+    <select onchange="{
+                const url = `{{ route('changeLang') }}`
+                window.location.href = url + '?lang=' + value;
+             }"
+            name="lang" id="languages" class="ml-4 bg-gray-50 text-gray-900 text-sm
                          focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-LightSlateGray-900
                          dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
-                         dark:focus:border-blue-500">
+                         dark:focus:border-blue-500" x-data="">
         <option value="en" {{ session()->get('locale') == 'en' ? 'selected' : '' }}>English</option>
         <option value="hu" {{ session()->get('locale') == 'hu' ? 'selected' : '' }}>Magyar</option>
     </select>
