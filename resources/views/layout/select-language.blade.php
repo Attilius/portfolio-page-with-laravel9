6e19a97dec6c9d71d6a17d8a461725a4e17c0c98
Attilius <leo20@freemail.hu>
2023-01-10 19:34:35 +0100

    I modified select-language.blade.php

--- resources/views/layout/select-language.blade.php
+++ resources/views/layout/select-language.blade.php
@@ -4,10 +4,10 @@
                 const url = `{{ route('changeLang') }}`
                 window.location.href = url + '?lang=' + value;
              }"
-            name="lang" id="languages" class="ml-4 bg-gray-50 text-gray-900 text-sm
-                         focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-LightSlateGray-900
-                         dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
-                         dark:focus:border-blue-500" x-data="">
+            name="lang" id="languages" class="lg:ml-6 xl:ml-12 bg-gray-50 text-gray-900 text-sm bg-slate-200
+                         focus:ring-blue-500 block p-2.5 dark:bg-LightSlateGray-900
+                         dark:placeholder-gray-400 dark:text-white hover:text-dark
+                         dark:text-gray-200 focus:outline-none">
         <option value="en" {{ session()->get('locale') == 'en' ? 'selected' : '' }}>English</option>
         <option value="hu" {{ session()->get('locale') == 'hu' ? 'selected' : '' }}>Magyar</option>
     </select>
