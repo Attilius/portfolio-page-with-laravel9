415d37ff9d10838d8db8045113f956c141e7db53
Attilius <leo20@freemail.hu>
2023-01-11 18:59:52 +0100

    I modified LanguageManager.php

--- app/Http/Middleware/LanguageManager.php
+++ app/Http/Middleware/LanguageManager.php
@@ -7,15 +7,19 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\App;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class LanguageManager
 {
     /**
      * Handle an incoming request.
      *
-     * @param  Request  $request
-     * @param  Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param Request $request
+     * @param Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return Response|RedirectResponse
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
      */
     public function handle(Request $request, Closure $next): Response|RedirectResponse
     {
