e46c329d089c31c2ab65de7ff22c93a3304414fb
Attilius <leo20@freemail.hu>
2023-01-13 19:57:39 +0100

    I modified page-not-found.blade.php

--- resources/views/page-not-found.blade.php
+++ resources/views/page-not-found.blade.php
@@ -1,4 +1,4 @@
-<div style="display: flex; align-items: center; justify-content: center;">
+<div style="display: flex; align-items: center; justify-content: center; height: 100%">
     <h1>404 Sorry the Page is not found!</h1>
 </div>
 
