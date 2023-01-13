5585035d5ab02c1c3de504139de02bc5e966c16b
Attilius <leo20@freemail.hu>
2023-01-13 14:20:33 +0100

    I modified PageNotFoundController.php

--- app/Http/Controllers/PageNotFoundController.php
+++ app/Http/Controllers/PageNotFoundController.php
@@ -8,6 +8,10 @@ class PageNotFoundController extends Controller
 {
     private static string $PATH = 'lang';
 
+    /**
+     * Display 404 Page not found site, when the url path is not exist.
+     *
+     */
     public function show(Request $request)
     {
         if (explode('/', $request->url())[3] !== self::$PATH) {
