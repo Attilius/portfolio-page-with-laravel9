2f08591485ad9b146091fc24839ba7fe77ea0952
Attilius <leo20@freemail.hu>
2023-01-15 14:45:11 +0100

    I modified page-not-found.blade.php

--- resources/views/components/page-not-found.blade.php
+++ resources/views/components/page-not-found.blade.php
@@ -4,7 +4,7 @@
             <h2 style="font-size: 8rem; line-height: 1; color: #dc2626; font-weight: 800;" class="mb-8">
                 404
             </h2>
-            <p style="font-size: 1.5rem;" class="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
+            <p style="font-size: 1.5rem; line-height: 2rem;" class="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
             <p class="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
             <a rel="noopener noreferrer" href="/" class="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</a>
         </div>
