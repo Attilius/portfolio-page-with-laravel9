a0c4a57c7149bcb036f59d65f699412fa03bdd50
Attilius <leo20@freemail.hu>
2023-01-16 18:04:35 +0100

    I modified page-not-found.php for (en)

--- lang/en/page-not-found.php
+++ lang/en/page-not-found.php
@@ -2,5 +2,6 @@
 
 return [
     "sorry" => "Sorry, we couldn't find this page.",
-    "opportunity" => "But don't worry, you can go back to the homepage."
+    "opportunity" => "But don't worry, you can go back to the homepage.",
+    "back_to_homepage" => "Back to homepage"
 ];
