ca09ea6d7840f817669ae7781a214a25ba9c630a
Attilius <leo20@freemail.hu>
2023-01-16 18:06:34 +0100

    I added page-not-found.php for (hu)

---
+++ lang/hu/page-not-found.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    "sorry" => "Sajnáljuk, nem találtuk ezt az oldalt.",
+    "opportunity" => "De ne aggódj, visszatérhetsz a kezdőlapra.",
+    "back_to_homepage" => "Vissza a főoldalra"
+];
