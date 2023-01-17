b6d7ba6e6421d4c2d1680c04fce8206cc7c82187
Attilius <leo20@freemail.hu>
2023-01-17 19:53:38 +0100

    I modified page-not-found.php for (en)

--- lang/en/page-not-found.php
+++ lang/en/page-not-found.php
@@ -3,5 +3,6 @@
 return [
     "sorry" => "I'm sorry but you seem to be lost.",
     "opportunity" => "Don't worry, you can go back to the homepage.",
-    "back_to_homepage" => "Back to homepage"
+    "back_to_homepage" => "Back to homepage",
+    "error" => "Error"
 ];
