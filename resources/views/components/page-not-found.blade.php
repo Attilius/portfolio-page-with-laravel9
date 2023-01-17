0c5ea6eaea18145175e59bcec6be5c38528deb49
Attilius <leo20@freemail.hu>
2023-01-17 19:49:35 +0100

    I modified page-not-found.blade.php

--- resources/views/components/page-not-found.blade.php
+++ resources/views/components/page-not-found.blade.php
@@ -1,14 +1,17 @@
-<div class="flex items-center min-h-screen p-16 dark:bg-LightSlateGray-900 dark:text-gray-100">
+<!-- ====== Page Not Found Section Start ====== -->
+<section class="flex items-center min-h-screen h-full p-16 bg-[url('/img/404_light.jpg')]
+                dark:bg-[url('/img/404_dark.jpg')] bg-no-repeat bg-cover dark:text-gray-100">
     <div class="container flex flex-col items-center justify-center px-5 mx-auto my-8">
-        <div style="max-width: 28rem;" class="text-center">
-            <h2 style="font-size: 8rem; line-height: 1; color: #dc2626; font-weight: 800;" class="mb-8">
-                404
+        <div class="max-w-md text-center">
+            <h2 class="mb-8 font-extrabold text-9xl text-red-600">
+                <span class="sr-only">Error</span>404
             </h2>
-            <p style="font-size: 1.5rem; line-height: 2rem; font-weight: 600;" class="md:text-3xl">{{ __('page-not-found.sorry') }}</p>
+            <p class="text-xl font-semibold md:text-2xl">{{ __('page-not-found.sorry') }}</p>
             <p class="mt-4 mb-8 dark:text-gray-400">{{ __('page-not-found.opportunity') }}</p>
             <x-button-link href="/" variant="primary" class="rounded-lg">
                 {{ __('page-not-found.back_to_homepage') }}
             </x-button-link>
         </div>
     </div>
-</div>
+</section>
+<!-- ====== Page Not Found Section END ====== -->
