d51c6b3ddde47f1697eec998171321a67eb06fa6
Attilius <leo20@freemail.hu>
2023-01-19 19:50:28 +0100

    I modified app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -18,7 +18,7 @@
         <script src="https://apis.google.com/js/platform.js"></script>
 
     </head>
-    <body class="antialiased text-gray-800 dark:text-gray-200">
+    <body class="antialiased bg-gray-200 dark:bg-slate-800 text-gray-800 dark:text-gray-200">
         <div class="@if($page_not_found) pt-0 @else pt-24 @endif min-h-screen bg-gray-100 dark:bg-gray-900">
             @if($page_not_found)
                 <x-page-not-found></x-page-not-found>
