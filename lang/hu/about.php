5a25a430e155c1cc35b33474cb66dc4998b5b5ea
Attilius <leo20@freemail.hu>
2023-01-20 20:03:06 +0100

    I modified about.php for (hu)

--- lang/hu/about.php
+++ lang/hu/about.php
@@ -5,6 +5,6 @@ return [
     "about_me" => "Rólam",
     "description1" => "Főiskolai tanulmányaim után 2020-ban a kialakult helyzet miatt úgy döntöttem, hogy programozói karrierbe kezdek. Érdeklődésem főként a következő programnyelvekre irányul",
     "and" => "és",
-    "description2" => "A programozásban új hobbit is találtam, mert sok kihívást rejt magában. A problémamegoldás az évek során szenvedélyemmé vált.",
+    "description2" => "A programozásban új hobbit is találtam, mert sok kihívást rejt magában. A problémamegoldás az évek során szenvedélyemmé vált. Egy másik hobbim a kerékpározás.",
     "experience" => "Tapasztalatok megtekintése"
 ];
