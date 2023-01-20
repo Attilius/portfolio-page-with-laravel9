250ef1fc49ecae03e20cd6898aa93d40b7c0a336
Attilius <leo20@freemail.hu>
2023-01-20 20:02:53 +0100

    I modified about.php for (en)

--- lang/en/about.php
+++ lang/en/about.php
@@ -5,6 +5,6 @@ return [
     "about_me" => "About Me",
     "description1" => "After my college studies in 2020, because of established the situation I decided to pursue a career in programming. My interest is mostly focused on",
     "and" => "and",
-    "description2" => "I've also found a new hobby in programming, because it's have many challenge. Problem solving has become a passion of mine over the years.",
+    "description2" => "I've also found a new hobby in programming, because it's has many challenge. Problem solving has become a passion of mine over the years. Another hobby is cycling.",
     "experience" => "View my experience"
 ];
