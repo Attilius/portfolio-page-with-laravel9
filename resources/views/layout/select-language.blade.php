d9777192b4d81ca34572668dea82596217ed23a7
Attilius <leo20@freemail.hu>
2023-01-21 19:38:26 +0100

    I modified select-language.blade.php

--- resources/views/layout/select-language.blade.php
+++ resources/views/layout/select-language.blade.php
@@ -19,7 +19,7 @@
                         <x-flag-language-hu class="w-8 h-8" />
                 @endif
                 <!-- Hero icon name: mini/chevron-down -->
-                <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <svg id="chevron_down" class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                     <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
                 </svg>
             </button>
@@ -39,9 +39,9 @@
                 const languageDropDownBox = document.getElementById('language_drop_down_box');
                 languageDropDownBox.classList.replace('visible', 'invisible');
                 }"
-               href="#" class="flex justify-center align-middle text-gray-700 block px-2 py-2 text-sm"
-               role="menuitem" tabindex="-1" id="menu-item-0">
-                <x-flag-language-en class="w-8 h-8" /> <span class="ml-2 pt-1 dark:text-white dark:hover:text-gray-400">English</span>
+               href="#" id="language-item-en" class="flex justify-center align-middle text-gray-700 block px-2 py-2 text-sm"
+               role="menuitem" tabindex="-1">
+                <x-flag-language-en class="w-8 h-8" /> <span id="lang_en" class="ml-2 pt-1 dark:text-white dark:hover:text-gray-400">English</span>
             </a>
             <a onclick="{
                 const url = `{{ route('changeLang') }}`
@@ -49,9 +49,9 @@
                 const languageDropDownBox = document.getElementById('language_drop_down_box');
                 languageDropDownBox.classList.replace('visible', 'invisible');
                 }"
-               href="#" class="flex justify-center align-middle text-gray-700 block px-2 py-2 text-sm"
-               role="menuitem" tabindex="-1" id="menu-item-1">
-                <x-flag-language-hu class="w-8 h-8" /> <span class="ml-2 pt-1 dark:text-white dark:hover:text-gray-400">Magyar</span>
+               href="#" id="language-item-hu" class="flex justify-center align-middle text-gray-700 block px-2 py-2 text-sm"
+               role="menuitem" tabindex="-1">
+                <x-flag-language-hu class="w-8 h-8" /> <span id="lang_hu" class="ml-2 pt-1 dark:text-white dark:hover:text-gray-400">Magyar</span>
             </a>
         </div>
     </div>
