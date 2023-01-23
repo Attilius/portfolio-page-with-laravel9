aaacebd09b45cc00aae012a818b0e37ad4f3875c
Attilius <leo20@freemail.hu>
2023-01-23 17:57:25 +0100

    I modified LanguageController.php

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -10,7 +10,7 @@ use Illuminate\View\View;
 class LanguageController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display the page in home section.
      *
      * @return View
      */
