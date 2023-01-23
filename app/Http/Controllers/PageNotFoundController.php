a27b82a645f7777f1f7c97e7e98cd334ce281e8a
Attilius <leo20@freemail.hu>
2023-01-23 17:52:19 +0100

    I modified PageNotFoundController.php

--- app/Http/Controllers/PageNotFoundController.php
+++ app/Http/Controllers/PageNotFoundController.php
@@ -11,7 +11,8 @@ class PageNotFoundController extends Controller
     private static string $PATH = 'lang';
 
     /**
-     * Display 404 Page not found site, when the url path is not exist.
+     * Display (404) Page not found site, when the url path is not exist.
+     * If the request path equals "lang" string then redirect into the home section.
      *
      *  @param Request $request
      *  @return RedirectResponse|View
