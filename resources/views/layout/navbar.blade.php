5caa41301fb6c0312ca6bfc842169edf647c2854
Attilius <leo20@freemail.hu>
2023-01-24 18:55:55 +0100

    I modified navbar.blade.php

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -27,7 +27,7 @@
                                 <!--Start of switch-->
                                 <div class="relative" x-on:click="darkMode = !darkMode">
                                     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                                         id="light-dark-switch"
+                                         id="dark_theme_switch"
                                          class="w-5 h-5 stroke-current"
                                          viewBox="0 0 48 48"
                                          style="fill:white; margin-left: 40px; cursor: pointer;">
