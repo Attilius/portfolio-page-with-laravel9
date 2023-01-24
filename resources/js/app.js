fe0bef24197e43483b32a90f6cc05613975fcd5e
Attilius <leo20@freemail.hu>
2023-01-24 19:01:19 +0100

    I modified app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -78,7 +78,6 @@ document.addEventListener('scroll', () => {
 
     if (displayThemeSwitchAction) {
       e.target.children[0].attributes[1].value = paths.moon;
-        checkHash();
     } else {
       e.target.children[0].attributes[1].value = paths.sun;
     }
