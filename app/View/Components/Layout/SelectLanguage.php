916ac55849314f19647f7fe6d93327d41363fa0f
Attilius <leo20@freemail.hu>
2023-01-29 21:46:45 +0100

    I modified SelectLanguage.php

--- app/View/Components/Layout/SelectLanguage.php
+++ app/View/Components/Layout/SelectLanguage.php
@@ -8,6 +8,11 @@ use Illuminate\Contracts\View\View;
 
 class SelectLanguage extends Component
 {
+    public function __construct()
+    {
+        //
+    }
+
     /**
      * Get the view / contents that represent the SelectLanguage component.
      *
